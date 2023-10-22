c8b64d43d142924baeae34c3a60c07c5ea5dca7f
bbangwon <bbangwon.chung@gmail.com>
2023-10-22 23:58:18 +0900

    초기 커밋.

---
+++ NetCore.Web/Controllers/MembershipController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore.Web.Controllers
+{
+    public class MembershipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]        
+        [ValidateAntiForgeryToken]
+        //위조방지토큰을 통해 View로부터 받은 Post Data가 유효한지 검증합니다.
+        public IActionResult Login(Models.LoginInfo loginInfo)
+        {
+            string message = string.Empty;
+            if (ModelState.IsValid)
+            {
+                string userId = "breadone";
+                string password = "123456";
+
+                if(loginInfo.UserId.Equals(userId) && loginInfo.Password.Equals(password))
+                {
+                    TempData["Message"] = "로그인에 성공했습니다.";
+                    return RedirectToAction("Index", "Membership");
+                }
+                else
+                {
+                    message = "로그인에 실패했습니다.";
+                }
+            }
+            else
+            {
+                message = "로그인 정보가 올바르지 않습니다.";
+            }
+
+            ModelState.AddModelError(string.Empty, message);
+            return View(loginInfo);
+        }
+        
+
+    }
+}
