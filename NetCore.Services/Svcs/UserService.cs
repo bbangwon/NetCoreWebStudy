ce4edc9ada7f31c8a44a695ae2d4b64705904a5c
bbangwon <bbangwon.chung@gmail.com>
2023-10-31 02:50:50 +0900

    EFCore - FromSql Method

--- NetCore.Services/Svcs/UserService.cs
+++ NetCore.Services/Svcs/UserService.cs
@@ -1,29 +1,76 @@
-﻿using NetCore.Data.DataModels;
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore.Data.DataModels;
 using NetCore.Data.ViewModels;
+using NetCore.Services.Data;
 using NetCore.Services.Interfaces;
 
 namespace NetCore.Services.Svcs
 {
     public class UserService : IUser
     {
+        CodeFirstDbContext _dbContext;
+
+        public UserService(CodeFirstDbContext context)
+        {
+            _dbContext = context;            
+        }
+
         #region Private Methods
         private IEnumerable<User> GetUserInfos()
         {
-            return new List<User>()
-            {
-                new User()
-                {
-                    UserId = "breadone",
-                    UserName = "빵원",
-                    UserEmail = "bbangwon.chung@gmail.com",
-                    Password = "123456"
-                }
-            };
+            return _dbContext.Users.ToList();
+            //return new List<User>()
+            //{
+            //    new User()
+            //    {
+            //        UserId = "breadone",
+            //        UserName = "빵원",
+            //        UserEmail = "bbangwon.chung@gmail.com",
+            //        Password = "123456"
+            //    }
+            //};
+        }
+
+        private User? GetUesrInfo(string userId, string password)
+        {
+            User? user;
+
+            //Lambda Expression
+            //user = _dbContext.Users.Where(u => u.UserId!.Equals(userId) && u.Password!.Equals(password)).FirstOrDefault();
+
+            //SQL
+            //user = _dbContext.Users
+            //    .FromSql($"SELECT UserId, UserName, UserEmail, Password, IsMembershipWithdrawn, JoinedUtcDate FROM dbo.[User]")
+            //    .Where(u => u.UserId!.Equals(userId) && u.Password!.Equals(password))
+            //    .FirstOrDefault();
+
+            //user = _dbContext.Users
+            //    .FromSql($"SELECT UserId, UserName, UserEmail, Password, IsMembershipWithdrawn, JoinedUtcDate FROM dbo.[User] WHERE UserId = {userId} AND Password = {password}")
+            //    .FirstOrDefault();
+
+            //VIEW
+            //user = _dbContext.Users
+            //    .FromSql($"SELECT UserId, UserName, UserEmail, Password, IsMembershipWithdrawn, JoinedUtcDate FROM dbo.[uvwUser]")
+            //    .Where(u => u.UserId!.Equals(userId) && u.Password!.Equals(password))
+            //    .FirstOrDefault();
+
+            //FUNCTION
+            user = _dbContext.Users
+                .FromSql($"SELECT * FROM dbo.[ufnUser]({userId}, {password})")
+                .FirstOrDefault();
+
+            //STORED PROCEDURE
+            //user = _dbContext.Users
+            //    .FromSql($"dbo.uspCheckLoginByUserId {userId}, {password}")
+            //    .AsEnumerable()
+            //    .FirstOrDefault();
+
+            return user;
         }
 
         private bool CheckTheUserInfo(string userId, string password)
         {
-            return GetUserInfos().Where(u => u.UserId!.Equals(userId) && u.Password!.Equals(password)).Any();
+            return GetUesrInfo(userId, password) != null;
         }
         #endregion
 
